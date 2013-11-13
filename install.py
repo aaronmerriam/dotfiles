e752a1e4ec68781aaea8398efaea2ab9f2d660d1
Piper Merriam <piper@simpleenergy.com>
2013-11-13 12:47:54 -0700

    pep8

--- install.py
+++ install.py
@@ -3,23 +3,22 @@ import os
 import os.path
 
 if __name__ == '__main__':
-  df_path = os.getcwd()
-  user_path = os.getenv('HOME')
+    df_path = os.getcwd()
+    user_path = os.getenv('HOME')
 
-  os.chdir(user_path)
+    os.chdir(user_path)
 
-  FILES = (
-    (os.path.isfile, ('vimrc', 'profile', 'gitignore', 'gitconfig', 'pythonrc.py', 'colemak.vim')),
-    (os.path.isdir, ('vim', )),
+    FILES = (
+        (os.path.isfile, ('vimrc', 'profile', 'gitignore', 'gitconfig', 'pythonrc.py', 'colemak.vim')),
+        (os.path.isdir, ('vim', )),
     )
-  stamp = int(time.time())
+    stamp = int(time.time())
 
-  for exists_method, file_list in FILES:
-    for df in file_list:
-      if exists_method(os.path.join(df_path, df)):
-        if os.path.islink(os.path.join(user_path, '.' + df)):
-          os.remove(os.path.join(user_path, '.' + df))
-        if exists_method(os.path.join(user_path, '.' + df)):
-          os.rename(os.path.join(user_path, '.' + df), os.path.join(user_path, '.' + str(stamp) + '.' + df))
-        os.symlink(os.path.join(df_path, df), '.' + df)
-
+    for exists_method, file_list in FILES:
+        for df in file_list:
+            if exists_method(os.path.join(df_path, df)):
+                if os.path.islink(os.path.join(user_path, '.' + df)):
+                    os.remove(os.path.join(user_path, '.' + df))
+                    if exists_method(os.path.join(user_path, '.' + df)):
+                        os.rename(os.path.join(user_path, '.' + df), os.path.join(user_path, '.' + str(stamp) + '.' + df))
+                        os.symlink(os.path.join(df_path, df), '.' + df)
